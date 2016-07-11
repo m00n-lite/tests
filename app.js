bbdb9a234b0de8597a11161cb7500f924ea498e6
Vadim Kulik <xbyte.com.ua@gmail.com>
2016-07-11 23:01:24 +0300

    start

---
+++ app.js
@@ -0,0 +1,20 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('FormController', function($scope, $http) {
+    
+    $scope.Send = function(){
+        var data = JSON.stringify($scope.form);
+        $http.post('mail.php', $scope.form, {headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    }).
+        success(function(data, status, headers, config) {
+            $scope.result = data;
+            console.log(data);
+        }).
+        error(function(data, status, headers, config) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+
+    }
+
+});
---
+++ mail.php
@@ -0,0 +1,16 @@
+<?
+	$_POST = json_decode(file_get_contents('php://input'), true);
+	$fields = ['name','phone','email','message'];
+	$errors=[];
+	foreach ($fields as $key) {
+		if($_POST[$key]==''){
+			$errors[$key]=$key. ' can not be null';
+		}
+	}
+	$ret = [];
+	$ret['status'] = 'true';
+	if(count($errors) > 0) $ret['status'] = 'false';
+	$ret['errors'] = 'false';
+	if(count($errors) > 0) $ret['errors'] = $errors;
+	echo json_encode($ret);
+?>
